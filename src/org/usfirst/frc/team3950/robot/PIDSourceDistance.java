e86c82fe4a19866b81f8a0e620bce82cc1ec070f
RoboGym <RoboGym@NorthShore.com>
2018-01-29 16:54:02 -0500

    PID Source Distance changes

---
+++ src/org/usfirst/frc/team3950/robot/PIDSourceDistance.java
@@ -0,0 +1,30 @@
+package org.usfirst.frc.team3950.robot;
+
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
+
+public class PIDSourceDistance implements PIDSource {
+
+	@Override
+	public void setPIDSourceType(PIDSourceType pidSource) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public PIDSourceType getPIDSourceType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double pidGet() {
+		// TODO Auto-generated method stub
+		return Robot.drivetrainSubsystem.getAverageEncoder();
+	}
+	
+	public void reset() {
+		Robot.drivetrainSubsystem.resetEncoders();
+	}
+
+}
