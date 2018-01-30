56547c977500d7a460b6407358d593702a636e80
Bryce Roth <bryceroth77@gmail.com>
2018-01-30 17:19:05 -0500

    Fixed PIDDistance output to smartdashboard

--- src/org/usfirst/frc/team3950/robot/commands/ScaleAutoCommand.java
+++ src/org/usfirst/frc/team3950/robot/commands/ScaleAutoCommand.java
@@ -7,7 +7,6 @@ import org.usfirst.frc.team3950.robot.TCS34725ColorSensor;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.PIDOutput;
-import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -40,8 +39,6 @@ public class ScaleAutoCommand extends Command implements PIDOutput{
 	
 	
     public ScaleAutoCommand() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
     	requires(Robot.drivetrainSubsystem);
     	source = new PIDSourceDistance();
     	pid = new PIDController(P, I, D, F, source, this);
@@ -102,7 +99,7 @@ public class ScaleAutoCommand extends Command implements PIDOutput{
     	SmartDashboard.putNumber("Left Encoder Distance", Robot.drivetrainSubsystem.getLeftEncoder());
     	SmartDashboard.putNumber("Right Encoder Distance", Robot.drivetrainSubsystem.getRightEncoder());
     	SmartDashboard.putNumber("Total Distance Travelled", Robot.drivetrainSubsystem.getCountDistanceFeet());
-		SmartDashboard.putNumber("Output", pid.get());
+		SmartDashboard.putNumber("Output", output);
     	Robot.drivetrainSubsystem.Drive(-output, 0);
 	}
 }
